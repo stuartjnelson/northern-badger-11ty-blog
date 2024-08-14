d61b8b7bb6adbc84b23a7e21ba0a247d88e06897
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-08-14 14:39:19 +0000

    hack: getting handlebars working

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
-const handlebars = require('handlebars')
+const handlebars = require('handlebars');
+const fs = require("fs");
+const path = require("path");
+
 const markdownIt = require("markdown-it");
 const markdownItAttrs = require('markdown-it-attrs');
 
@@ -43,6 +46,25 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.setLibrary('hbs', handlebars)
 
 
+	// ======
+	// Had to manually register handlebars partials. Without this inital startung if the app meant
+	// that `index.md` could not find any partials. 
+	// @TODO: Replace handlebars with something else
+	// Define the partials directory
+	const partialsDir = path.join(__dirname, "includes");
+
+	// Register each partial in the partials directory with Handlebars
+	fs.readdirSync(partialsDir).forEach(file => {
+	  const matches = /^([^.]+).hbs$/.exec(file);
+	  if (!matches) {
+		return;
+	  }
+	  const name = matches[1];
+	  const template = fs.readFileSync(path.join(partialsDir, file), "utf8");
+	  handlebars.registerPartial(name, template);
+	});
+
+
 	// Handlebar helpers
 	eleventyConfig.addHandlebarsHelper("jsonPrint", obj => JSON.stringify(obj, null, 2))
 	eleventyConfig.addHandlebarsHelper("addOne", num => (num + 1))
@@ -59,6 +81,12 @@ module.exports = (eleventyConfig) => {
 			.join('-')
 	})
 
+	// @NOTE: Leaving in here in case need this in the future
+	// Add a custom logger to debug partial paths
+	// eleventyConfig.on('eleventy.before', async ({dir, runModa, outputMode}) => {
+	// 	console.log("🚨🚨🚨 dir 🚨🚨🚨", dir);
+	// 	console.log("Registered Handlebars partials:", handlebars);
+	// });
 
 
 
@@ -87,19 +115,19 @@ module.exports = (eleventyConfig) => {
 	// ============================================
 	// HTML transforms
 	// ============================================
-	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
-		if (outputPath.endsWith(".html")) {
-			let minified = htmlmin.minify(content, {
-				useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true
-			});
-			return minified;
-		}
+	// eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+	// 	// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+	// 	if (outputPath.endsWith(".html")) {
+	// 		let minified = htmlmin.minify(content, {
+	// 			useShortDoctype: true,
+	// 			removeComments: true,
+	// 			collapseWhitespace: true
+	// 		});
+	// 		return minified;
+	// 	}
 
-		return content;
-	});
+	// 	return content;
+	// });
 
 
 	/**
@@ -107,78 +135,78 @@ module.exports = (eleventyConfig) => {
 	 *
 	 * Inserts `<section>` into articles using `h2` to signify a new section
 	 */
-	eleventyConfig.addTransform('create-article-sections', (content, outputPath) => {
-		let returnContent = content;
+	// eleventyConfig.addTransform('create-article-sections', (content, outputPath) => {
+	// 	let returnContent = content;
 
-		if (outputPath.endsWith('.html') && returnContent.includes('id="article-content"')) {
-			// 0. Get opening article element
-			const openingArticleTag = returnContent.match(/<article.*?>/)[0];
+	// 	if (outputPath.endsWith('.html') && returnContent.includes('id="article-content"')) {
+	// 		// 0. Get opening article element
+	// 		const openingArticleTag = returnContent.match(/<article.*?>/)[0];
 
-			// 1. Get the start of the article
-			const articleContentArray = returnContent.split(/<article.*?>/);
-			// 1.1 Get all HTML before the article
-			const preArticleContent = articleContentArray[0];
+	// 		// 1. Get the start of the article
+	// 		const articleContentArray = returnContent.split(/<article.*?>/);
+	// 		// 1.1 Get all HTML before the article
+	// 		const preArticleContent = articleContentArray[0];
 
-			// 2. Need to get only the article content
-			let articleContent = articleContentArray[1];
-			const postContent = articleContent.split(/<\/article>/);
+	// 		// 2. Need to get only the article content
+	// 		let articleContent = articleContentArray[1];
+	// 		const postContent = articleContent.split(/<\/article>/);
 
-			// 2.1 This is all content after the article
-			let mainContent = postContent[0];
-			const postArticleContent = postContent[1];
+	// 		// 2.1 This is all content after the article
+	// 		let mainContent = postContent[0];
+	// 		const postArticleContent = postContent[1];
 
-			// 3. Getting all headings inside article
-			const contentArray = postContent[0].split(/<h2.*>(.*)<\/h2>/);
+	// 		// 3. Getting all headings inside article
+	// 		const contentArray = postContent[0].split(/<h2.*>(.*)<\/h2>/);
 
-			// 4. If there is more than a single "section" there is more to do
-			if (contentArray.length > 3) {
-				// 4.1 For first section we need to wrap a `<section>` element around it
-				const preFirstH2 = `
-					<section>
-						${contentArray[0]}
-					</section>
-				`;
+	// 		// 4. If there is more than a single "section" there is more to do
+	// 		if (contentArray.length > 3) {
+	// 			// 4.1 For first section we need to wrap a `<section>` element around it
+	// 			const preFirstH2 = `
+	// 				<section>
+	// 					${contentArray[0]}
+	// 				</section>
+	// 			`;
 
-				mainContent = contentArray
-					// 4.1 We want to remove first item from the array
-					.filter((item, index) => index > 0)
-					// 4.2 Reducing over the array to create other sections
-					.reduce((acc, curr, index) => {
-						let processedHtml = '';
+	// 			mainContent = contentArray
+	// 				// 4.1 We want to remove first item from the array
+	// 				.filter((item, index) => index > 0)
+	// 				// 4.2 Reducing over the array to create other sections
+	// 				.reduce((acc, curr, index) => {
+	// 					let processedHtml = '';
 
-						// 4.3 Is it an even item and not the only "section"
-						if (index % 2 === 0) {
-							// 4.3.1 Getting text for the heading
-							const headingText = curr;
+	// 					// 4.3 Is it an even item and not the only "section"
+	// 					if (index % 2 === 0) {
+	// 						// 4.3.1 Getting text for the heading
+	// 						const headingText = curr;
 
-							// 4.3.2 Getting HTML for the section
-							const sectionContent = contentArray[index + 2];
+	// 						// 4.3.2 Getting HTML for the section
+	// 						const sectionContent = contentArray[index + 2];
 
-							// 4.3.3  Creating the HTML for the section
-							processedHtml = `
-								<section>
-									<h2>${headingText}</h2>
-									${sectionContent}
-								</section>
-							`;
-						}
+	// 						// 4.3.3  Creating the HTML for the section
+	// 						processedHtml = `
+	// 							<section>
+	// 								<h2>${headingText}</h2>
+	// 								${sectionContent}
+	// 							</section>
+	// 						`;
+	// 					}
 
-						return `${acc}${processedHtml}`;
-					}, preFirstH2);
-			}
+	// 					return `${acc}${processedHtml}`;
+	// 				}, preFirstH2);
+	// 		}
 
-			// 5. Recomposing the HTML for the page
-			returnContent = `
-				${preArticleContent}
-				${openingArticleTag}
-					${mainContent}
-				</article>
-				${postArticleContent}
-			`;
-		}
+	// 		// 5. Recomposing the HTML for the page
+	// 		returnContent = `
+	// 			${preArticleContent}
+	// 			${openingArticleTag}
+	// 				${mainContent}
+	// 			</article>
+	// 			${postArticleContent}
+	// 		`;
+	// 	}
 
-		return returnContent;
-	})
+	// 	return returnContent;
+	// })
 
 
 	/**
