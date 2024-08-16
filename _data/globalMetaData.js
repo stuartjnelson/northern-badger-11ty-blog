9277b49a725eba784bbcb67345fdb7f47946cca7
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-08-16 08:33:36 +0000

    fix: adding global data file

---
+++ _data/globalMetaData.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+	return {
+		description: "Northern badger blog - frontend web development blog focusing on a11y, accessablity, scss, HTML and performance",
+		baseUrl: "https://www.blog.northernbadger.co.uk"
+	};
+};
